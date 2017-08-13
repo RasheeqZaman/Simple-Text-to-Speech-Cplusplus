e84be280742ec81762d3c5af6b1a1fe8c28c4dbc
RasheeqZaman <zamanrshq@gmail.com>
2017-08-13 10:27:10 +0600

    banano shesh

---
+++ SimpleTTS/SimpleTTS/Form1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Speech
+Imports System.Speech.Synthesis
+
+
+Public Class Form1
+
+    Dim speaker As New SpeechSynthesizer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        speaker.SpeakAsync(RichTextBox1.Text)
+        Label1.Text = RichTextBox1.Text
+    End Sub
+End Class
